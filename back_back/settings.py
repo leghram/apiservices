4d0d0e5d1fdc44548ae8166316e8aa51816b53af
Miguel PL <mig.parlop@gmail.com>
2022-11-22 20:14:48 -0500

    customizing

--- back_back/settings.py
+++ back_back/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from os import environ
 from dotenv import load_dotenv
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(d$&7ag&b5&%c_yv+t=f$!277=rt0y$m=!=0m(nj%&d+nd816q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,7 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
